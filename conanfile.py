bf118037ce54d1fa305b666d5076125533c1ff9e
Arne Mertz <github@arne-mertz.de>
2016-08-07 12:35:27 +0200

    bump library version

--- conanfile.py
+++ conanfile.py
@@ -3,13 +3,17 @@ from conans.tools import download, unzip
 
 class NLJsonConan(ConanFile):
     name = "nlJson"
-    version = "2.0.1"
+    version = "2.0.2"
     url = "https://github.com/arnemertz/conan-nlJson.git"
     license = "MIT"
     author = "Arne Mertz (arne-mertz.de/contact-me)"
+    settings = None #header only
 
     def source(self):
 	download("https://github.com/nlohmann/json/releases/download/v%s/json.hpp" % self.version, "json.hpp")
+
+    def build(self):
+        None #header only
 
     def package(self):
         self.copy("*.hpp", dst="include")
