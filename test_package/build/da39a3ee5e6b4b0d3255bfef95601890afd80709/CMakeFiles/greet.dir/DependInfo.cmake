6e639ef74602ff4082a2ea9659e23dd1d51611b0
Arne Mertz <github@arne-mertz.de>
2016-11-05 13:51:21 +0100

    add package test

---
+++ test_package/build/da39a3ee5e6b4b0d3255bfef95601890afd80709/CMakeFiles/greet.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vagrant/git/conan-nlJson/test_package/main.cpp" "/home/vagrant/git/conan-nlJson/test_package/build/da39a3ee5e6b4b0d3255bfef95601890afd80709/CMakeFiles/greet.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_GLIBCXX_USE_CXX11_ABI=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/vagrant/.conan/data/nlJson/2.0.7/arnemertz/stable/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test_package/build/da39a3ee5e6b4b0d3255bfef95601890afd80709/CMakeFiles/greet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greet.dir/main.cpp.o"
+  "bin/greet.pdb"
+  "bin/greet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
